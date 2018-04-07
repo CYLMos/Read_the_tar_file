732883495f2bd40d6ee0a7118829609cf77a4549
LCY <cylin910021@gmail.com>
2018-04-07 21:17:10 +0800

    Add the feature to check whether the file is the USTART format or not

--- main.cpp
+++ main.cpp
@@ -9,17 +9,20 @@ int main(int argc, char *argv[])
 {
     /// If the number of the params is wrong
     if(argc >= 3){
-        cout << "To many params" << endl;
+        cout << "Too many params" << endl;
         exit(0);
     }
     else if(argc <= 1){
-        cout << "To less params" << endl;
+        cout << "Too less params" << endl;
         exit(0);
     }
 
     MyTar myTar(argv[1]);
 
-    myTar.startRead();
+    if(!myTar.startRead()){
+	cout << "Not USTART file!" << endl;
+	exit(0);
+    }
     myTar.showContent();
 
     return 0;
